cf06520400dc59540b53abb4e9689d89568da8a3
Rollin Gilman <rdgilman@my.canyons.edu>
2026-04-01 18:55:50 -0700

    a57 Fibonacci

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@ int main()
 	int n1, n2;
 	int N, sum;
 
+	cout << "Enter two numbers: ";
 	cin >> n1 >> n2;
+	cout << "Enter the number of sequences: ";
 	cin >> N;
+	cout << n1 << "\t" << n2 << "\t";
 
 	// TODO
+	for (int i=0; i< N-2; i++){
+		sum = n1 + n2;
+		cout << sum << "\t";
+		n1 = n2;
+		n2 = sum;
+	}
 	// TODO
+	cout << endl;
 }
